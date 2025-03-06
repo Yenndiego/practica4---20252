281385998c4696b0a582c9e640e15df813fde41d
Yenndiego <jaindiego6@gmail.com>
2025-03-06 14:28:44 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,21 @@
+
+//Diego Jain Espinoza
+//ejercicio 1 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int a = 3;
+    int b = 4;
+    if (a>b)
+    {
+        printf("a es mayor h \n");
+        
+    }
+    else
+    {
+        printf("b es mayor o igual a \n");
+    }
+    return 0;
+}
